5d49c64ba8c8d2c0f4eeae4ff2bb611f442bde80
fibeapp <motiw@me.com>
2023-01-28 08:16:07 +0600

    Убрал Storyboard

--- WorkoutApp/ViewController.swift
+++ WorkoutApp/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  WorkoutApp
-//
-//  Created by Vladimir Fibe on 1/28/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,7 +6,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
